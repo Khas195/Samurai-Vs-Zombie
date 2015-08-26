83eb7cc77ea33f38a4de47a72f8c99ce42bc6774
Khas195 <gamer1091995@yahoo.com>
2015-08-26 09:11:13 +0700

    CharacterUI

---
+++ Assets/Characters/CharacterStats.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+
+    public float maxHealth;
+    float curHealth;
+    public float attackDamage;
+
+    public delegate void OnHealthChange(float health);
+    public OnHealthChange onHealthReduced;
+	// Use this for initialization
+	void Start () {
+        curHealth = maxHealth;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    public void SufferDamage(float damage)
+    {
+        curHealth -= damage;
+        if (onHealthReduced != null)
+            onHealthReduced(curHealth);
+
+    }
+    public void Die()
+    {
+        // Heroes's Death
+        Debug.Log("Heroes's Death");
+    }
+}
---
+++ Assets/UI/HealthBarControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBarControl : MonoBehaviour {
+
+    public CharacterStats mChar;
+    public Text mTextPercentage;
+
+    public GameObject healthBar; // cache for visibleHealth
+    private Image healthBarImage;
+    private float maxHealth; // cache max health
+	// Use this for initialization
+	void Start () {
+
+        mChar.onHealthReduced += HandleHealthChange;
+        healthBarImage = healthBar.GetComponent<Image>();
+        maxHealth = mChar.maxHealth;
+
+	}
+	public void HandleHealthChange(float newHealth)
+    {
+        float healthPercentage = newHealth / maxHealth;
+        if (healthPercentage < 0)
+            return;
+        mTextPercentage.text = healthPercentage*100 + "%";
+        healthBar.transform.localScale = new Vector3(healthPercentage, 1, 1);
+        Debug.Log("healthPercentage: " + healthPercentage);
+        if (healthPercentage >= .4 && healthPercentage <= .6 )
+        {
+            healthBarImage.color = new Color32(200, 255, 0, 255);// yellow-ish
+        }
+        else if ( healthPercentage < .4)
+        {
+            healthBarImage.color = new Color32(255, 0, 0, 255);// red
+        }
+        else
+        {
+            Debug.Log("Green Health");
+            healthBarImage.color = new Color32(0, 255, 0, 255); // green
+        }
+
+    }
+}

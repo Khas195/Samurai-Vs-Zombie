5c60a8e0e5fdb32b15c063c909b135067eb5cc55
chanh <crazyboy6103@gmail.com>
2015-08-26 09:50:35 +0700

    Enemy AI undone

---
+++ Assets/Decrepit Dungeon/Prefabs/Doorways/Door.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    bool isOpen = false;
+	// Use this for initialization
+	void Start () {
+        isOpen = true;
+        this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
